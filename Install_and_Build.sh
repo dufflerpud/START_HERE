492611c1861c3c4b4745dd5bf228d6014539728c
Christopher Caldwell <dufflerpud@yahoo.com>
2026-02-28 21:59:08 -0500

    WEBOFFSET debugging

--- Install_and_Build.sh
+++ Install_and_Build.sh
@@ -31,21 +31,24 @@
 #doc#	Install_and_Build.sh - A script to download and install all of the dufflerpud projects
 ########################################################################
 
-PROG=`basename $0`
+PROG=`basename $0 .sh`
 USER=`id -un`
 GROUP=`id -gn`
-WUSER=www-data
-WGROUP=www-data
-WEBTOP=/var/www/html/sto
-
-FS0_IP=10.1.0.20
-FS0_NAME=fs0
-SSHFILES=chris@${FS0_IP}:.ssh/github.com
-REF_FILES=chris@${FS0_IP}:vm_stuff/ref.cpio
-SSHDIR=/home/$USER/.ssh
+WUSER=`awk -F: '/^(apache|www-data)/ {print $3}' /etc/passwd`
+WGROUP=`awk -F: '/^(apache|www-data)/ {print $4}' /etc/passwd`
 
 PROJECTS_DIR=/usr/local/projects
 BE_CLEAN=false
+DEVELOPER=false
+if [ -f /etc/cpi_cfg.pl ] ; then
+    WEBOFFSET=`perl -e 'eval(\`cat /etc/cpi_cfg.pl\`); print $cpi_vars::WEBOFFSET;'`
+else
+    WEBOFFSET=/`date +%s | cksum -a sha1 --base64 --untagged | tr -d +/= | cut -c1-4`
+fi
+SUDO_HACK=/etc/sudoers.d/$PROG
+
+LIKE_DEBIAN=false;	[ -x /bin/apt ] && LIKE_DEBIAN=true
+LIKE_REDHAT=false;	[ -x /bin/dnf ] && LIKE_REDHAT=true
 
 #########################################################################
 #	Echo command and then do it.					#
@@ -54,6 +57,16 @@ echodo()
     {
     echo "+ $@" >&2
     "$@"
+    }
+
+#########################################################################
+#	Turn off sudo asking passwords for the duration of the script.	#
+#	There is a special place in hell for me.  I know it.		#
+#########################################################################
+make_sudo_friendly()
+    {
+    echo '%wheel ALL=(ALL) NOPASSWD: ALL' | \
+	sudo install -o root -g root -m 0444 /dev/stdin $SUDO_HACK
     }
 
 #########################################################################
@@ -83,8 +96,29 @@ echocd()
 performa_updates()
     {
     echo "[ Performa updates ]"
-    ecsudo apt update -yq
-    ecsudo apt upgrade -yq
+    if [ -x /bin/dnf ] ; then
+        ecsudo dnf -y update
+    elif [ -x /bin/apt ] ; then
+	ecsudo apt update -yq
+	ecsudo apt upgrade -yq
+    else
+        echo "Unable to update this OS, continuing."
+    fi
+    }
+
+#########################################################################
+#	Use the right installation tool					#
+#########################################################################
+osinstall()
+    {
+    if [ -x /bin/dnf ] ; then
+        ecsudo dnf -y install $*
+    elif [ -x /bin/apt ] ; then
+        ecsudo apt install -qqy $*
+    else
+        echo "Unable to install:  " $*
+	exit 1
+    fi
     }
 
 #########################################################################
@@ -93,8 +127,13 @@ performa_updates()
 setup_projects()
     {
     echo "[ Setting up projects ]"
-    ecsudo apt install -qqy make gcc translate-shell libjpeg-dev
-    ecsudo apt install -qqv sox ghostscript netpbm poppler-utils
+    osinstall script make gcc translate-shell
+    osinstall sox ghostscript netpbm poppler-utils cpan
+    $LIKE_DEBIAN && osinstall libjpeg-dev
+    if [ ! -e /usr/lib/sendmail ] ; then
+	osinstall ssmtp
+	$LIKE_DEBIAN && osinstall mailutils
+    fi
     ecsudo PERL_MM_USE_DEFAULT=1 cpan -i CPAN
     ecsudo cpan install -i Imager/File/JPEG.pm
     ecsudo install -d -m 0755 -o root -g root ${PROJECTS_DIR}
@@ -107,19 +146,23 @@ setup_projects()
 install_and_configure_apache2_web_server()
     {
     echo "[ Installing and configuring Apache web server ]"
-    ecsudo apt install -qqy apache2
+    osinstall apache2
     #tmog l - - - /etc/apache2/mods-enabled/cgi.load ../mods-available/cgi.load
     [ -h /etc/apache2/mods-enabled/cgi.load ] || \
 	ecsudo ln -s ../mods-available/cgi.load /etc/apache2/mods-enabled/cgi.load
     #tmog l - - - /etc/apache2/conf-enabled/serve-cgi-bin.conf ../conf-available/serve-cgi-bin.conf
     [ -h /etc/apache2/conf-enabled/serve-cgi-bin.conf ] || \
 	ecsudo ln -s ../conf-available/serve-cgi-bin.conf /etc/apache2/conf-enabled/serve-cgi-bin.conf
-    ecsudo install -d -m 0755 -o ${WUSER} -g ${WGROUP} ${WEBTOP}
-    ecsudo ed -s /etc/apache2/apache2.conf <<EDEOF
+    grep 'AddHandler cgi-script' /etc/apache2/apache2.conf > /dev/null || ecsudo ed -s /etc/apache2/apache2.conf <<EDEOF
 	/<Directory \/var\/www\/
 	a
 	AddHandler cgi-script .cgi .pl
 .
+	w
+	q
+EDEOF
+    grep 'Options ExecCGI' /etc/apache2/apache2.conf > /dev/null || ecsudo ed -s /etc/apache2/apache2.conf <<EDEOF
+	/<Directory \/var\/www\/
 	/Options
 	s/Options/Options ExecCGI/
 	w
@@ -128,6 +171,74 @@ EDEOF
     ecsudo systemctl enable apache2
     ecsudo systemctl start apache2
     ecsudo systemctl reload apache2	# This should really not be needed
+    }
+
+#########################################################################
+#	Install and configure the httpd web server			#
+#	Pretty Redhat dependent.					#
+#########################################################################
+install_and_configure_httpd_web_server()
+    {
+    echo "[ Installing and configuring httpd web server ]"
+    osinstall httpd
+    if grep 'AddHandler cgi-script' /etc/httpd/conf.d/cgi.conf > /dev/null 2>&1 ; then
+	:
+    else
+        echo "AddHandler cgi-script .cgi .pl" | \
+	    ecsudo dd of=/etc/httpd/conf.d/cgi.conf oflag=append
+    fi
+    grep 'Options .*ExecCGI' /etc/httpd/conf/httpd.conf > /dev/null 2>&1 || \
+	ecsudo ed -s /etc/httpd/conf/httpd.conf <<EDEOF
+	/<Directory "\/var\/www\/
+	/Options
+	s/Options/Options +ExecCGI/
+	w
+	q
+EDEOF
+    ecsudo systemctl enable httpd.service
+    ecsudo systemctl start httpd.service
+    ecsudo systemctl reload httpd.service	# This should really not be needed
+    }
+
+#########################################################################
+#	Decide what web server is appropriate and get it going.		#
+#########################################################################
+install_and_configure_a_web_server()
+    {
+    if $LIKE_REDHAT ; then
+	install_and_configure_httpd_web_server
+    else
+	install_and_configure_apache2_web_server
+    fi
+    for DOCUMENTROOT in /var/www/www /var/www/html ; do
+	if [ -d $DOCUMENTROOT ] ; then
+	    WEBTOP=$DOCUMENTROOT$WEBOFFSET
+	    break
+	fi
+    done
+
+    if [ -z "$WEBTOP" ] ; then
+	echo "No documentroot found."
+	exit 1
+    fi
+
+    if [ ! -f /etc/cpi_cfg.pl ] ; then
+	tmpfile=/tmp/$PROG.$$
+        sed -e 's/^	*//' > $tmpfile <<EOF
+		#\$cpi_vars::WEBOFFSET="YourDomain.com";
+		#\$cpi_vars::FAX_SERVER="Your fax printer name";
+		#\$cpi_vars::KEY_CAPTCHA_PUBLIC="Captcha public key";
+		#\$cpi_vars::KEY_CAPTCHA_PRIVATE="Captcha private key";
+		\$cpi_vars::WEBOFFSET="$WEBOFFSET";
+EOF
+	ecsudo install -o root -g root -m 0644 $tmpfile /etc/cpi_cfg.pl
+	rm $tmpfile
+    fi
+
+    ecsudo install -d -m 0755 -o ${WUSER} -g ${WGROUP} ${WEBTOP}
+    ecsudo firewall-cmd --zone=public --add-service=http --permanent
+    ecsudo systemctl reload firewalld.service
+    echo "[Web software will be installed into ${WEBTOP}]"
     }
 
 #########################################################################
@@ -149,11 +260,20 @@ git_clone_to()
 
 #########################################################################
 #	Install generic project software.				#
+#	Note that if no url is provided, we'll see if we can ssh it.	#
+#	Otherwise, we'll use the public address.			#
 #########################################################################
 install_and_configure()
     {
     project="$1"
-    url=${2:-"https://github.com/dufflerpud/$project.git"}
+    url="$2"
+    if [ -z "$url" ] ; then
+        if [ -r "$HOME/.ssh/github.com/$project-ro.pub" ] ; then
+	    url="git@github.com-$project-ro:dufflerpud/$project.git"
+	else
+    	    url="https://github.com/dufflerpud/$project.git"
+	fi
+    fi
     echo "[ Installing and configuring $project ]"
     top_proj_dir=$PROJECTS_DIR/$project
     git_clone_to "$url" "$top_proj_dir"
@@ -167,44 +287,7 @@ install_and_configure()
 #########################################################################
 setup_communication()
     {
-    echo "[ Setting up communication ]"
-    githubdir="$SSHDIR/github.com"
-    echodo rm -rf $githubdir
-    echodo mkdir -p $githubdir
-    echodo scp -q "$SSHFILES/*-ro" $githubdir
-    echodo cd $githubdir
-    for sshkeyfile in * ; do
-	githubhost="github.com-$sshkeyfile"
-	echo "host $githubhost"
-	echo "    Hostname github.com"
-	echo "    User git"
-	echo "    IdentityFile $githubdir/$sshkeyfile"
-	echo "    IdentitiesOnly yes"
-    done > $SSHDIR/config
-    echodo chmod -R go-rwx $SSHDIR/config $githubdir
-
-    if grep -s $FS0_NAME /etc/hosts >/dev/null; then
-	echo "/etc/hosts already updated."
-    else
-	echodo scp -q ${FS0_IP}:/etc/hosts /tmp/hosts.0
-	echodo grep -v '^127' < /tmp/hosts.0 > /tmp/hosts.1
-	ecsudo dd if=/tmp/hosts.1 of=/etc/hosts oflag=append conv=notrunc
-    fi
-    }
-
-#########################################################################
-#	E-mail configuration						#
-#########################################################################
-install_and_configure_email()
-    {
-    tmp=/tmp/$PROG
-    echo "[ Setting up e-mail ]"
-    ecsudo apt install -qqy ssmtp mailutils
-    echodo scp $REF_FILES $tmp
-    echocd /
-    ecsudo cpio -iduv < $tmp
-    ecsudo chown root:root `cpio -it < $tmp
-    ecsudo chmod 644 `cpio -it < $tmp
+    ssh 10.1.0.20 sh /usr/local/projects/START_HERE/developer.sh | sh
     }
 
 #########################################################################
@@ -226,12 +309,22 @@ usage()
     }
 
 #########################################################################
+#	Get rid of the travesty we created for doing things as root.	#
+#########################################################################
+cleanup()
+    {
+    ecsudo rm -f $SUDO_HACK
+    }
+
+#########################################################################
 #	Main								#
 #########################################################################
 
 while [ "$#" -gt 0 ] ; do
     case "$1" in
-	-clean)	BE_CLEAN=true					;;
+	-c*)	BE_CLEAN=true					;;
+	-d*)	DEVELOPER=true					;;
+	-w*)	WEBOFFSET="$2"; shift				;;
 	*)	PROBLEMS="${PROBLEMS}Unknown argument [$1]~"	;;
     esac
     shift
@@ -239,10 +332,14 @@ done
 
 [ -z "$PROBLEMS" ] || usage "$PROBLEMS"
 
-$BE_CLEAN && ecsudo rm -rf ${WEBTOP} ${PROJECTS_DIR} /etc/cpi_cfg.pl /etc/ssmtp/ssmtp.conf
+make_sudo_friendly
+trap cleanup EXIT
 
 performa_updates
-install_and_configure_apache2_web_server
+osinstall git
+install_and_configure_a_web_server
+
+$BE_CLEAN && ecsudo rm -rf ${WEBTOP} ${PROJECTS_DIR} /etc/cpi_cfg.pl /etc/ssmtp/ssmtp.conf
 
 setup_projects
 install_and_configure cpi		# Requires setup_projects
@@ -268,12 +365,15 @@ install_and_configure Visas		# Requires cpi & common
 install_and_configure activist		# Requires cpi & common
 install_and_configure sign		# Requires cpi & common
 
-ecsudo apt -qqy install f2c libncurses-dev
+osinstall f2c
+$LIKE_DEBIAN && osinstall libncurses-dev
+$LIKE_REDHAT && osinstall ncurses-devel
 install_and_configure multis		# Requires cpi
 
 install_and_configure cci		# Requires common
 install_and_configure pandemic		# Requires cpi, common and cci
 
-#install_and_configure websh		# Requires cpi & common
-#setup_communication
-#install_and_configure_email
+if $DEVELOPER ; then
+    setup_communication
+    install_and_configure websh		# Requires cpi & common
+fi
